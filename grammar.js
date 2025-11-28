0ffe4df67fb92b2b85485da86e851518c7011b3c
Wes Higbee <wes.mcclure@gmail.com>
2025-11-28 17:13:04 -0600

    inline $final.token too! this is just a grouping in my grammar

--- grammar.js
+++ grammar.js
@@ -14,6 +14,7 @@ module.exports = grammar({
     $.anything_without_hoovering_tags,
     $.header_assistant, // not an actual node, just use child header node types directly
     $.header,
+    $.final_token,
   ],
 
   // FYI extras: [\s] is default... allows for whitespace around tokens unless clear it to force exact matches below...
